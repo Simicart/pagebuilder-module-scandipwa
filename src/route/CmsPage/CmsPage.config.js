4c5bb11452eaa85b3b09d7a919ccaa03ab7101bd
lanapone <lanapone@lanapones-MBP.lan>
2021-08-10 16:38:14 +0700

    another clean up

--- src/route/CmsPage/CmsPage.config.js
+++ src/route/CmsPage/CmsPage.config.js
@@ -1,6 +1,2 @@
 export const endPoint = 'https://tapita.io/pb/graphql/';
 export const maskedId = '15N9yEIPKmY8Cwks3ZDu191627462948';
-// export const maskedId = '15lvnZMFx4olnIlh5x4Q5n1628147164';
-// export const maskedId = '153TN2aQmOghjJNG6qVfDO1628504617';
-
-
