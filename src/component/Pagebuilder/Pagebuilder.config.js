47ac0faaccc7f4039e7211c54d97841d0518f483
lanapone <lanapone@lanapones-MBP.lan>
2021-08-11 13:31:27 +0700

    clean up

--- src/component/Pagebuilder/Pagebuilder.config.js
+++ src/component/Pagebuilder/Pagebuilder.config.js
@@ -1,5 +1,3 @@
 export const endPoint = 'https://tapita.io/pb/graphql/';
-export const maskedId = '15N9yEIPKmY8Cwks3ZDu191627462948';
 export const integrationToken = '150kG2pgFhmxb6zJVJFSyTAV4oAV1JXc1623205870';
 export const storeCode = ''
-export const LOADING_TIME = 300
