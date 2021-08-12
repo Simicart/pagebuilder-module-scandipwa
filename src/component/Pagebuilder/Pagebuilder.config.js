3fc6c29e31c3aadf2c804ee9fe3008b59f272640
Cody Nguyen <cody@simicart.com>
2021-08-12 20:27:01 +0700

    Update token

--- src/component/Pagebuilder/Pagebuilder.config.js
+++ src/component/Pagebuilder/Pagebuilder.config.js
@@ -1,3 +1,3 @@
 export const endPoint = 'https://tapita.io/pb/graphql/';
-export const integrationToken = '150kG2pgFhmxb6zJVJFSyTAV4oAV1JXc1623205870';
+export const integrationToken = '30pz20IZtTscKFZkxeSha7PskUPgFBFR1628772496';
 export const storeCode = ''
