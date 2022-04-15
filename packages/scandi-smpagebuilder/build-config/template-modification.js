bcebd18cf529a33e91be334218700ce652dca4a9
lanapone <lanapone@lanapones-MacBook-Pro.local>
2022-04-15 11:12:34 +0700

    fix: unset default font size for tapita components

--- packages/scandi-smpagebuilder/build-config/template-modification.js
+++ packages/scandi-smpagebuilder/build-config/template-modification.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const fs = require('fs');
 module.exports = {
     templatePlugin: {
         overrideDOM({
@@ -5,11 +7,16 @@ module.exports = {
             parser,
             serializer
         }) {
+            const styling = fs
+                .readFileSync(
+                    path.resolve('packages/scandi-smpagebuilder/build-config/assets/globalStyle.css'), 'utf8'
+                );
+
             const additionalHeadElements = [
                 // eslint-disable-next-line max-len
                 '<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">',
-                `<style>
-                * {font-family: 'Poppins', Helvetica, Arial, sans-serif !important;font-size: 14px;line-height: 1.5;}
+                `<style class="globalStyle">
+                    ${styling}
                 </style>`
             ];
 
