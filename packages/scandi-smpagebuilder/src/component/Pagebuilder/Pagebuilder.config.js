40181fd798751b60f4acf8e4dfa239aaac92547a
alitonia <allyofjustice1@gmail.com>
2022-07-08 09:23:17 +0700

    remove log

--- packages/scandi-smpagebuilder/src/component/Pagebuilder/Pagebuilder.config.js
+++ packages/scandi-smpagebuilder/src/component/Pagebuilder/Pagebuilder.config.js
@@ -1,4 +1,3 @@
-// export const domain = 'https://tapita.io/';
-export const domain = 'https://tapitaio.pwa-commerce.com/';
+export const domain = 'https://tapita.io/';
 export const endPoint = `${domain}pb/graphql/`;
 export const integrationToken = '150kG2pgFhmxb6zJVJFSyTAV4oAV1JXc1623205870';
--- packages/scandi-smpagebuilder/src/plugin/CategoryPage.component.plugin.js
+++ packages/scandi-smpagebuilder/src/plugin/CategoryPage.component.plugin.js
@@ -5,7 +5,6 @@ import {CategoryLayout} from "../component/Pagebuilder/components/LayoutTapita/C
 export const CategoryPageScarecrow = (args, callback, instance) => {
     const t = callback()
 
-    console.log('teeee',instance)
     return (
         <>
             <CategoryLayout instance={instance} layoutFilter={1}/>
