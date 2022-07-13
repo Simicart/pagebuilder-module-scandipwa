0236ac09c8c1668598e2c2869431a1a89a9a8dfd
alitonia <allyofjustice1@gmail.com>
2022-07-13 15:19:57 +0700

    Restore domain

--- packages/scandi-smpagebuilder/src/component/Pagebuilder/Pagebuilder.config.js
+++ packages/scandi-smpagebuilder/src/component/Pagebuilder/Pagebuilder.config.js
@@ -1,5 +1,3 @@
-// export const domain = 'https://tapita.io/';
-export const domain = 'https://tapitaio.pwa-commerce.com/';
+export const domain = 'https://tapita.io/';
 export const endPoint = `${domain}pb/graphql/`;
-// export const integrationToken = '150kG2pgFhmxb6zJVJFSyTAV4oAV1JXc1623205870';
-export const integrationToken = '228EAej34FSd85WyIOZqgpqafIf9zoZiv1642218428';
+export const integrationToken = '150kG2pgFhmxb6zJVJFSyTAV4oAV1JXc1623205870';
