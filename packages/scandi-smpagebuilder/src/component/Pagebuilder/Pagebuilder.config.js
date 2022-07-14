564109d0b39969d498f447f2fa76463053590021
alitonia <allyofjustice1@gmail.com>
2022-07-14 16:14:13 +0700

    remove log

--- packages/scandi-smpagebuilder/src/component/Pagebuilder/Pagebuilder.config.js
+++ packages/scandi-smpagebuilder/src/component/Pagebuilder/Pagebuilder.config.js
@@ -1,4 +1,3 @@
 export const domain = 'https://tapita.io/';
 export const endPoint = `${domain}pb/graphql/`;
-// export const integrationToken = '150kG2pgFhmxb6zJVJFSyTAV4oAV1JXc1623205870';
-export const integrationToken = '938SOSZNhHPB2FcVLdS8v6n85dPMgaT0y1649837967';
+export const integrationToken = '150kG2pgFhmxb6zJVJFSyTAV4oAV1JXc1623205870';
--- packages/scandi-smpagebuilder/src/plugin/HomePage.container.plugin.js
+++ packages/scandi-smpagebuilder/src/plugin/HomePage.container.plugin.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {HomePageContainer} from '../component/route/HomePage/HomePage.container';
 
 export const HomePageScarecrow = (args, callback, instance) => {
-    console.log('talle rerenderign')
     return (
         <HomePageContainer {...instance.props} />
     );
